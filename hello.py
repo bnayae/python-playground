f52462955e0a725e680e5235e17219f31e3f3471
Bnaya Eshet <3382669+bnayae@users.noreply.github.com>
2023-03-22 09:32:34 +0200

    first commit

---
+++ hello.py
@@ -0,0 +1,6 @@
+# https://code.visualstudio.com/docs/python/python-tutorial
+# https://wiki.python.org/moin/BeginnersGuide/Programmers
+# https://python.land/python-tutorial
+msg = "hello python"
+print(msg)
+print("Hi")
